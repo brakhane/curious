520766f4de0699e46cd26aa86520951e0214bcd8
Dennis Brakhane <brakhane@gmail.com>
2019-08-19 21:10:40 +0200

    bump asks version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
         "pylru==1.0.9",
         "oauthlib>=2.0.2,<2.1.0",
         "pytz>=2017.3",
-        "asks>=1.3.0,<1.4.0",
+        "asks>=2.3.0,<2.4.0",
         "multidict>=4.1.0,<4.2.0",
         "multio>=0.2.1,<0.3.0",
         "async_generator~=1.9",  # asynccontextmanager for 3.6
