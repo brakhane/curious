06e081c26ff66e4c436e2c33ed8e91776c65c7a1
Dennis Brakhane <brakhane@gmail.com>
2019-08-25 23:54:31 +0200

    replace lomond with trio-websocket

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ install_requires = [
         "multidict>=4.1.0,<4.2.0",
         "multio>=0.2.1,<0.3.0",
         "async_generator~=1.9",  # asynccontextmanager for 3.6
-        "typing_inspect>=0.2.0"
+        "typing_inspect>=0.2.0",
+        "trio_websocket>=0.8.0,<0.9.0"
 ]
 
 py36_requires = [
